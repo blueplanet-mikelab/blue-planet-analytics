26208e7289c0bc4e3211d8145a0fecb4f72fb83b
Vittunyuta <vittunyuta@gmail.com>
2019-10-24 15:48:31 +0700

    finish create theme vectors

--- python/threadsByHand.py
+++ python/threadsByHand.py
@@ -7,6 +7,7 @@ import re
 import math
 import pymongo
 import os
+import datetime
 
 
 # prepare stopwords list
@@ -64,7 +65,6 @@ def cleanContent(rawContent):
     content = re.sub(r'Ý', 'Y', content) #14 replace extended chars y
     content = re.sub(r'ñ|Ñ', 'n', content) #15 replace extended chars n
     content = re.sub(r'ß', 's', content) #16 replace extended chars n
-    # spechar = r'(\s+)|"|:|!|ๆ|~|#|@|\*|(\?)|\(|\)|\[|\]|\-|\+|•|\_|\/|>|<|^|(?<!\d)\.(?!\d)|(5{5,})|\=|\”|\“|♡|(?<!\d)\,(?!\d)|&|¥|฿|✿|([^a-zA-Z0-9ก-๙\s]+)'
     spechar = r'[^a-zA-Z0-9ก-๙\.\,\s]+|\.{2,}|\xa0+|\d+[\.\,][^\d]+'
     content = re.sub(spechar, ' ', content) #17 remove special character
     #18 remove duplicate characters and spaces
@@ -104,6 +104,10 @@ def computeTF( freqDictList):
 # idf = ln(total number of dics/number of docs with term in it)
 def computeIDF(freqDictList, fname='IDFScoreByWord.json'):
     helperDict = {} # keep idf score which have already computed
+    if os.path.isfile(fname):
+        with open('./'+fname,'r', encoding="utf8") as json_file:
+            helperDict = json.load(json_file)
+
     IDF_scores_docs = []
     for idx, freqDict in enumerate(freqDictList):
         print(idx, "----", freqDict['topic_id'])
@@ -157,87 +161,164 @@ def computeTFIDF(TF_scores, IDF_scores):
 
     return TFIDF_scores_docs
 
-def computeDF(tfidfList, fname='keysDFScores.json'):
-   DF_scores_docs = {}
-   for idx, tfidfTopic in enumerate(tfidfList):
-      print(idx, "---", tfidfTopic['_id'])
-      for scores in tfidfTopic["tfidf_scores"]:
-         currentWord = scores['key']
-         if currentWord not in DF_scores_docs.keys():
-            count = 0 # number of docs that havs the key
-            for tempTopic in tfidfList:
-               for k in tempTopic["tfidf_scores"]:
-                  if k['key'] == currentWord:
-                     count += 1
-                     break
-            dfScore = count/len(tfidfList)
-            DF_scores_docs[currentWord] = dfScore
+def computeDF(tfidfList, fname):
+    DF_scores_docs = {}
+    for idx, tfidfTopic in enumerate(tfidfList):
+        print(idx, "---", tfidfTopic['topic_id '])
+        for scores in tfidfTopic["tfidf_scores"]:
+            currentWord = scores['key']
+            if currentWord not in DF_scores_docs.keys():
+                count = 0 # number of docs that havs the key
+                for tempTopic in tfidfList:
+                    for k in tempTopic["tfidf_scores"]:
+                        if k['key'] == currentWord:
+                            count += 1
+                            break
+                dfScore = count/len(tfidfList)
+                DF_scores_docs[currentWord] = dfScore
 
-   # write output txt file
-   with open('./'+fname, 'w', encoding="utf8") as outfile:
-      json.dump(DF_scores_docs, outfile, ensure_ascii=False, indent=4)
-   
-   return DF_scores_docs
+    # write output txt file
+    with open('./'+fname, 'w', encoding="utf8") as outfile:
+        json.dump(DF_scores_docs, outfile, ensure_ascii=False, indent=4)
+    
+    return DF_scores_docs
+
+def removeAndWriteFile(fname, content, ftype='json'):
+   if os.path.isfile(fname):
+      os.remove(fname)
+      print("remove",fname,"success")
+
+   if ftype == 'txt':
+      f = open(fname, "w",encoding='utf-8')
+      f.write(content)
+      print("create",fname,"success")
+      f.close() 
+   elif ftype == 'json':
+      with open('./'+fname, 'w', encoding="utf8") as outfile:
+         json.dump(content, outfile, ensure_ascii=False, indent=4)
+         print("create",fname,"success")
+   else:
+      print("invalid file type")
 
 if __name__ == "__main__":
-    client = pymongo.MongoClient("hostname",
-                                27017,
-                                username='username',
-                                password='password',
-                                authSource='admin' )
-    db = client["dbname"] #TODO database access
+    # client = pymongo.MongoClient("hostname",
+    #                             27017,
+    #                             username='username',
+    #                             password='password',
+    #                             authSource='admin' )
+    # db = client["dbname"] #TODO database access
 
-    # read csv
+    #! 0. read csv -> threadsList
     with open('./labeledThreadsbyHand.csv', 'r', encoding="utf8") as f:
-        threads = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
+        threadsList = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
+        removeAndWriteFile('0-threads100.json', threadsList)
 
+    print("----------Word Summary-----------")
     freqDictList = []
     # pprint.pprint(threads)
     # pprint.pprint(threads[0])
-    threadsCount = len(threads)
-    for idx, thread in enumerate(threads):
+    threadsCount = len(threadsList)
+    for idx, thread in enumerate(threadsList):
         topicID = thread['TopicID']
-
+        print(idx,"--",topicID)
+        
         with urllib.request.urlopen("http://ptdev03.mikelab.net/kratooc/"+topicID) as url:
             threadData = json.loads(url.read().decode())
             # print(threadData)
 
-        #TODO 1. retrieve title+destription+comment
+        #! 1. retrieve title+destription+comment
         title = threadData['_source']['title']
         # print("title:",title)
         desc = threadData['_source']['desc']
         # print("desc:",desc)
         userID = threadData['_source']['uid']
-        comments = [comment['desc'] for comment in threadData['_source']['comments'] if comment['uid']==userID] # TODO comments not clean
+        comments = [comment['desc'] for comment in threadData['_source']['comments'] if comment['uid']==userID]
         # print("comments:")
-        # print('\n'.join(comments))
+        # print(' '.join(comments))
         rawContent = title + desc + ' '.join(comments)
 
-        #TODO 2. tokenize+wordsummary
+        #! 2. tokenize+wordsummary
+        rawContent = re.sub(r'<[^<]+/>|\\.{1}|&[^&]+;|\n|\r\n','', rawContent) # to msg_clean
+        # removeAndWriteFile('test.txt',cleanContent(rawContent),'txt')
         wordsSum, tokensLength = createWordsSummary(cleanContent(rawContent), getStopWords("./stopwords_more_th.txt"))
-
-        #TODO 3. push to mongo
-        wordsum_col = db["word_summary"]
+        # freqDictList.append({"topic_id": topicID, "words_sum": wordsSum, "tokens_length": tokensLength, "created_at":datetime.datetime.now()})
         freqDictList.append({"topic_id": topicID, "words_sum": wordsSum, "tokens_length": tokensLength})
-        result = wordsum_col.insert_many(freqDictList)
-        print("result--",result)
 
-        break
-    
-    #TODO 4. TFIDF calculation
-    tfidf_col = db["scores_tfidf"]
+
+    #! 3. push to mongo   
+    # wordsum_col = db["word_summary"]
+    # result = wordsum_col.insert_many(freqDictList)
+    # print("result--",result)
+    removeAndWriteFile('1-wordsummary.json', freqDictList)
+
+    #! 4. TFIDF calculation
+    # tfidf_col = db["scores_tfidf"]
+    print("----------TF-----------")
     tfScores = computeTF(freqDictList)
+    print("----------IDF-----------")
     idfScores = computeIDF(freqDictList)
+    print("----------TF_IDF-----------")
     tfidfScores = computeTFIDF(tfScores,idfScores)
-    result = tfidf_col.insert_many(tfidfScores)
-    print("result--",result)
+    removeAndWriteFile('2-tfidfScores.json', tfidfScores)
+    # result = tfidf_col.insert_many(tfidfScores)
+    # print("result--",result)
 
-    #TODO 5. Vector calculation
+    #! 5. Vector calculation
+    print("----------vector creation-----------")
     dfScoresfname = "keysDFScores.json"
     if os.path.isfile(dfScoresfname):
-      with open('./'+dfScoresfname,'r', encoding="utf8") as json_file:
-         dfScoresDict = json.load(json_file)
+        with open('./'+dfScoresfname,'r', encoding="utf8") as json_file:
+            dfScoresDict = json.load(json_file)
     else:
         dfScoresDict = computeDF(tfidfScores, fname=dfScoresfname)
     
-    #TODO 5.2
+    #! 5.2 Words selection
+    themesVector = []
+    # at least 1 theme and no more than 4 per thread -> only count more than 5 per theme
+    for i, thread in enumerate(threadsList):
+        topicID = thread["TopicID"]
+        tokens = [[key['key'] for key in tfidf["tfidf_scores"]] for tfidf in tfidfScores if tfidf["topic_id"]==topicID][0]
+        
+        minn = 0.0006
+        wordsBag = [[key['key'] for key in tfidf["tfidf_scores"] if key['score'] > minn] for tfidf in tfidfScores if tfidf["topic_id"]==topicID][0]
+        print(i,"--",topicID,"--", len(tokens), "->", len(wordsBag),"--m:", minn)
+        # print(wordsBag)
+        
+        threadsThemeList = thread["Theme"].replace(" ","").split(",") #string
+        for idx, theme in enumerate(threadsThemeList):
+            if not [vec for vec in themesVector if vec['type']==theme]: #create new theme
+                themesVector.append({'type':theme, 'topic_ids':[topicID], 'word_vectors':{}})
+            
+            for vector in themesVector: # select which theme to add
+            # print("vector theme:",vector['type'])
+                if vector['type'] == theme:
+                    vector["topic_ids"].append(topicID)
+                
+                    for word in wordsBag:
+                        # print("word:",word)
+                        if word not in vector['word_vectors'].keys():
+                            # print("word not found in key")
+                            vector['word_vectors'][word] = dfScoresDict[word] if word in dfScoresDict.keys() else 0
+                        # if word has already in 'word_vectors', it don't need to do anything.
+                    break # there is only one theme vector per theme
+        # break # 1 thread
+    
+    removeAndWriteFile('3-themesVector'+minn+'.json',themesVector,'json')
+
+    #! 5.3 Sorted
+    for vector in themesVector:
+        print("----theme:", vector['type'])
+        print("before sorted:", len(vector["word_vectors"]))
+        vector["word_vectors"] = [{'word':k,'val': v} for k, v in sorted(vector["word_vectors"].items(), key=lambda x: x[1], reverse=True)]
+        print("after sorted:", len(vector["word_vectors"]))
+        # vector["created_at"] = datetime.datetime.now()
+   
+    fname = '4-sortedThemesVector.json'
+    removeAndWriteFile(fname,themesVector,'json')
+
+    # vector_col = db["vector_theme"]
+    # vector_col.drop()
+    # result = vector_col.insert_many(themesVector)
+    # print(result)
+
+
